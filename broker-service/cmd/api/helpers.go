c13b96783ce804d3c9f6d0e3aa6b5cf558941457
nuttchai <nuttchai@outlook.com>
2022-06-16 01:50:06 +0700

    Creating some helper functions to deal with JSON and such

---
+++ broker-service/cmd/api/helpers.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	maxBytes := 1048576 // 1MB
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes)) // 1MB max body size
+
+	decoder := json.NewDecoder(r.Body) // decoder reads from request body
+	err := decoder.Decode(data)        // decode JSON into data
+	if err != nil {
+		return err
+	}
+
+	// check whether there's only a json value in the file we received or not
+	err = decoder.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must have only a single JSON value")
+	}
+
+	return nil
+}
+
+func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header ...http.Header) error {
+	out, err := json.Marshal(data) // marshal data into JSON
+	if err != nil {
+		return err
+	}
+
+	if len(header) > 0 {
+		for key, value := range header[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out) // write JSON to response
+
+	return err
+}
+
+func (app *Config) errorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return app.writeJSON(w, statusCode, payload)
+}
