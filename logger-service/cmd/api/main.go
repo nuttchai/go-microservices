b83c2d418d2917b1337e88f957b0a27a0407529d
nuttchai <nuttchai@outlook.com>
2022-06-19 03:52:09 +0700

    Build logger service

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoUrl = "mongodb://localhost:27017"
+	gRcpPort = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+}
+
+func main() {
+	// connect to mongo
+	client, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// create a context in order to disconnect
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	// close connection
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// create connection options
+	clientOptions := options.Client().ApplyURI(mongoUrl)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting:", err)
+		return nil, err
+	}
+
+	// check the connection
+	err = c.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Println("Error pinging:", err)
+		return nil, err
+	}
+
+	return c, nil
+}
